b20c4c7661ad2debc29f6082784125de9b177382
DoctorCnC <jaylim070@gmail.com>
2023-06-10 16:12:15 +0500

    Item detalis working had an error in models

--- controllers/category.js
+++ controllers/category.js
@@ -1,52 +1,4 @@
-// const category = require("../models/category")
-// const asyncHandler = require("express-async-handler")
-
-
-
-
-
-// exports.index = asyncHandler(async (req, res, next) => {
-//   res.send("NOT IMPLEMENTED: Site Home Page");
-
-// // Display list of all categorys.
-// exports.category_list = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category list");
-//   });
-  
-//   // Display detail page for a specific category.
-//   exports.category_detail = asyncHandler(async (req, res, next) => {
-//     res.send(`NOT IMPLEMENTED: category detail: ${req.params.id}`);
-//   });
-  
-//   // Display category create form on GET.
-//   exports.category_create_get = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category create GET");
-//   });
-  
-//   // Handle category create on POST.
-//   exports.category_create_post = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category create POST");
-//   });
-  
-//   // Display category delete form on GET.
-//   exports.category_delete_get = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category delete GET");
-//   });
-  
-//   // Handle category delete on POST.
-//   exports.category_delete_post = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category delete POST");
-//   });
-  
-//   // Display category update form on GET.
-//   exports.category_update_get = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category update GET");
-//   });
-  
-//   // Handle category update on POST.
-//   exports.category_update_post = asyncHandler(async (req, res, next) => {
-//     res.send("NOT IMPLEMENTED: category update POST");
-//   });
+const { body, validationResult } = require("express-validator");
 
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
@@ -60,7 +12,7 @@ exports.index = asyncHandler(async (req, res, next) => {
   // Retrieve category data
   const categories = await Category.find({}).exec();
 
-  res.render("category/index", {
+  res.render("index", {
     title: "All Categories",
     category_count: numCategories,
     categories: categories,
@@ -84,9 +36,46 @@ exports.category_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle category create on POST.
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category create POST");
-});
+// Handle category create on POST.
+// exports.category_create_post = [
+//   // Validate and sanitize the name field.
+//   body("name", "category name must contain at least 3 characters")
+//     .trim()
+//     .isLength({ min: 3 })
+//     .escape(),
+
+//   // Process request after validation and sanitization.
+//   asyncHandler(async (req, res, next) => {
+//     // Extract the validation errors from a request.
+//     const errors = validationResult(req);
+
+//     // Create a category object with escaped and trimmed data.
+//     const category = new category({ name: req.body.name });
+
+//     if (!errors.isEmpty()) {
+//       // There are errors. Render the form again with sanitized values/error messages.
+//       res.render("category_form", {
+//         title: "Create category",
+//         category: category,
+//         errors: errors.array(),
+//       });
+//       return;
+//     } else {
+//       // Data from form is valid.
+//       // Check if category with same name already exists.
+//       const categoryExists = await category.findOne({ name: req.body.name }).exec();
+//       if (categoryExists) {
+//         // category exists, redirect to its detail page.
+//         res.redirect(categoryExists.url);
+//       } else {
+//         await category.save();
+//         // New category saved. Redirect to category detail page.
+//         res.redirect(category.url);
+//       }
+//     }
+//   }),
+// ];
+
 
 // Display category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
